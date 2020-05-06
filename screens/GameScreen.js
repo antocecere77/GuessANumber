9af06ee14f0392b0f0c9f4d4201c445a8e2495ec
antocecere <antocecere77@gmail.com>
2020-05-06 07:55:18 +0200

    Finishing the game logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -8,12 +8,12 @@ const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
-    if(rndNum === exclude) {
-        return generateRandomBetween(min, max, exclude);
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
     } else {
-        return rndNum;
+      return rndNum;
     }
-};
+  };
 
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
