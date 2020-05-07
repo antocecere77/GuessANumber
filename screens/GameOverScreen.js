50dd86df5d4feae8e632d2afcf405eb60628a54f
antocecere <antocecere77@gmail.com>
2020-05-07 11:41:00 +0200

    Calculating sizes dynamically

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button, Image } from 'react-native';
+import { View, StyleSheet, Text, Button, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -9,6 +9,7 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
+      <ScrollView>
         <View style={styles.screen}>
             <TitleText>The Game is Over!</TitleText>
             <View style={styles.imageContainer}>
@@ -26,6 +27,7 @@ const GameOverScreen = props => {
             </View>            
             <MainButton onPress={props.onRestart}>NEW GAME</MainButton> 
         </View>
+      </ScrollView>
     );
 };
 
@@ -36,13 +38,13 @@ const styles = StyleSheet.create({
       alignItems: 'center'
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
     borderWidth: 3,
     borderColor: 'black',
     overflow: 'hidden',
-    marginVertical: 30 
+    marginVertical: Dimensions.get('window').height / 30  
   },
   image: {
     width: '100%',
@@ -50,11 +52,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
       marginHorizontal: 30,
-      marginVertical: 15
+      marginVertical: Dimensions.get('window').height / 60  
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20
   },
   highlight: {
       color: Colors.primary,
